57e6a1690ca4d87d64766147ad6b9873ca4af281
boazglick <95533522+boazglick@users.noreply.github.com>
2022-06-07 14:19:05 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteUrl: "https://buzz-tv.co.il", // Site domain. Do not include a trailing slash!
+  siteUrl: "https://alankaler.co.il", // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: "buzz-tv", // This allows an alternative site title for meta data for pages.
+  siteTitleMeta: "alankaler", // This allows an alternative site title for meta data for pages.
   siteDescriptionMeta:
-    "buzz-tv11", // This allows an alternative site description for meta data for pages.
+    "alankaler", // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: "buzz-tv11", // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: "alankaler", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#15171A", // Used for Offline Manifest
   apiUrl: "https://wordpress.theasdfghjkl.com",
   subscribeWidget: {
     visible: true,
-    title: "buzz-tv",
+    title: "alankaler",
     helpText: "הישארו מעודכנים",
     successMessage: "תודה ההודעה נשלחה בהצלחה",
   },
@@ -25,11 +25,11 @@ module.exports = {
     navigation: [
       {
         label: "דף הבית",
-        url: "https://buzz-tv.co.il/",
+        url: "https://alankaler.co.il/",
       },
       {
         label: "יצירת קשר",
-        url: "https://buzz-tv.co.il/contact",
+        url: "https://alankaler.co.il/contact",
       },
     ],
   },
@@ -38,16 +38,16 @@ module.exports = {
     navigation: [
       {
         label: "דף הבית",
-        url: "https://buzz-tv.co.il/",
+        url: "https://alankaler.co.il/",
       },
       {
         label: "Sitemap",
-        url: "https://buzz-tv.co.il/sitemap.xml",
+        url: "https://alankaler.co.il/sitemap.xml",
       },
       
       {
         label: "יצירת קשר",
-        url: "https://buzz-tv.co.il/contact",
+        url: "https://alankaler.co.il/contact",
       },
      
     ],
@@ -70,9 +70,9 @@ module.exports = {
     successMessage: "We’ll get in touch with you soon.",
   },
   metadata: {
-    title: "buzz-tv",
+    title: "alankaler",
     description:
-      "buzz-tv55",
+      "alankaler",
   },
   twitterCard: {
     title: "",
@@ -88,9 +88,9 @@ module.exports = {
     imageUrl: "facebookImage.png",
     appId: "2391725224459953",
   },
-  siteTitle: "buzz-tv",
+  siteTitle: "alankaler",
   siteDescription:
-    "buzz-tv66",
+    "alankaler",
   language: "he",
   logoUrl: "logo.png",
   iconUrl: "favicon.png",
